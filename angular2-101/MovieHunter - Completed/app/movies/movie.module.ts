44090b1b02dbff18008a94aad53fd912386c507d
Deborah Kurata <deborahk@insteptech.com>
2016-08-29 12:04:51 -0700

    Rearranged the contents of the modules as per recommendations.

--- angular2-101/MovieHunter - Completed/app/movies/movie.module.ts
+++ angular2-101/MovieHunter - Completed/app/movies/movie.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+
+import { SharedModule } from '../shared/shared.module';
 
 import { movieRouting } from './movie.routing';
 import { MovieListComponent } from './movie-list.component';
@@ -8,12 +8,8 @@ import { MovieDetailComponent } from './movie-detail.component';
 import { MovieFilterPipe } from './movie-filter.pipe';
 import { MovieService } from './movie.service';
 
-import { SharedModule } from '../shared/shared.module';
-
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
     SharedModule,
     movieRouting
   ],
