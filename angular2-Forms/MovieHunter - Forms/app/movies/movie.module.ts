9e0e45b821e64dc3481353df1c7fd130bd9591bc
Deborah Kurata <deborahk@insteptech.com>
2016-08-29 17:04:28 -0700

    Code for Angular 2 Forms and Validation talk.

--- angular2-Forms/MovieHunter - Forms/app/movies/movie.module.ts
+++ angular2-Forms/MovieHunter - Forms/app/movies/movie.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SharedModule } from '../shared/shared.module';
 
 import { movieRoutingModule } from './movie.routing';
 import { MovieListComponent } from './movie-list.component';
 import { MovieDetailComponent } from './movie-detail.component';
+import { MovieEditComponent } from './movie-edit.component';
+import { MovieEditReactiveComponent } from './movie-edit-reactive.component';
 import { MovieFilterPipe } from './movie-filter.pipe';
 import { MovieService } from './movie.service';
 
 @NgModule({
   imports: [
+    ReactiveFormsModule,
     SharedModule,
     movieRoutingModule
   ],
   declarations: [
     MovieListComponent,
     MovieDetailComponent,
+    MovieEditComponent,
+    MovieEditReactiveComponent,
     MovieFilterPipe
   ],
   providers: [
---
+++ angular2-Forms/MovieHunter - Forms/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule }  from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { StarComponent } from './star.component';
+import { RangeValidator } from './range.directive';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    StarComponent,
+    RangeValidator
+  ],
+  exports: [
+    StarComponent,
+    RangeValidator,
+    CommonModule,
+    FormsModule ]
+})
+export class SharedModule { }
