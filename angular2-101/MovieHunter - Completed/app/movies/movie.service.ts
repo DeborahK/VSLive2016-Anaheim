495d57eee59c61a3d27460c7c3ecc9a9df5dd535
Deborah Kurata <deborahk@insteptech.com>
2016-09-01 08:47:07 -0700

    Update to RC 6.

--- angular2-101/MovieHunter - Completed/app/movies/movie.service.ts
+++ angular2-101/MovieHunter - Completed/app/movies/movie.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
 
